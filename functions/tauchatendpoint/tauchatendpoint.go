06180c2b204bc3b4901eebbc35f519f06c83af8c
Aldanis Vigo <aldanisvigo@gmail.com>
2023-03-25 05:37:54 -0500

    tauchatendpoint code

---
+++ functions/tauchatendpoint/tauchatendpoint.go
@@ -0,0 +1,49 @@
+package lib
+
+import (
+	"fmt"
+
+	"github.com/taubyte/go-sdk/event"
+	"github.com/taubyte/go-sdk/pubsub/node"
+)
+
+func chat(e event.Event) error {
+	h, err := e.HTTP()
+
+	if err != nil {
+		return err
+	}
+
+	channel, err := node.Channel("chat")
+	if err != nil {
+		return err
+	}
+
+	url, err := channel.WebSocket().Url()
+	if err != nil {
+		return err
+	}
+
+	_, err = h.Write([]byte("{\"socket\" : \"" + url.Path + "\"}"))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//export tauchatendpoint
+func Tauchatendpoint(e event.Event) uint32 {
+	h, err := e.HTTP()
+
+	if err != nil {
+		h.Write([]byte(fmt.Sprintf("ERROR: %s\n", err)))
+	}
+
+	err = chat(e)
+	if err != nil {
+		return 1
+	}
+
+	return 0
+}
